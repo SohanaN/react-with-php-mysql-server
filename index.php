30b11729a2557abdfa8f6860f941064b945b2fe0
SohanaN <sohananaz01@gmail.com>
2023-03-27 00:17:12 +0600

    to show data in list page

--- index.php
+++ index.php
@@ -1,13 +1,26 @@
 <?php
+    // error_reporting(E_All);
+    // ini_set('display_errors',1);
+
     header('Access-Control-Allow-Origin: *');
     header("Access-Control-Allow-Headers: *");
-    
+
+    // Reference database connection class file and connect to MySQL Database
     include("DbConnect.php");
     $conn = new DbConnect();
     $db = $conn->connect();
+    // Give you Method used to hit API
     $method = $_SERVER['REQUEST_METHOD'];
     switch($method) {
+        case 'GET':
+            $sql = "SELECT * FROM users";
+            $stmt = $db->prepare($sql);
+            $stmt->execute();
+            $users = $stmt->fetchAll(PDO::FETCH_ASSOC);        
+            echo json_encode($users);
+            break;
         case 'POST':
+            // Read the POST JSON data and convert it into PHP Object
             $user = json_decode(file_get_contents('php://input'));
             $sql = "INSERT INTO users(id, name, email, mobile, created_at) values(null, :name, :email, :mobile, :created_at)";
             $stmt = $db->prepare($sql);
